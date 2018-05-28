870288e2ea9336db876b6aaa45eefa607786d4bd
cdcalderon <cdcalderon@gmail.com>
2018-05-27 19:07:27 -0500

    configure login passport cookies

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-
+require('./models/User');
 const mongoDB = mongoose.connect(keys.mongoURI);
 
 require('./services/passport');
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
