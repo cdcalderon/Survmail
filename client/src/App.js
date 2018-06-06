7030e7f505fa0fc6f1756a2310612d7b12dbfce2
cdcalderon <cdcalderon@gmail.com>
2018-06-05 22:58:38 -0500

    Add server proxy configuration

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <a href="http://localhost:5000/auth/google">Sign in with Google</a>
+          //https://mysterious-bastion-20584.herokuapp.com/
+          <a href="/auth/google">Sign in with Google</a>
       </div>
     );
   }
